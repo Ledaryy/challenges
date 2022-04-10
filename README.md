4056f2e8611b7f8bfa52d56d1f52623ddeeb8741
Ledaryy <andrew.ledary@gmail.com>
2022-04-11 00:55:33 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# There are all my public coding_challenges/leetcodes/mocks/architecture_design in this repo.
+
+# Current version of python: 3.10.2
+
+# Leetcodes:
+1. [EASY][13. Roman to Integer](https://leetcode.com/problems/roman-to-integer/)[repo]()
---
+++ leetcode/1/main.py
@@ -0,0 +1,38 @@
+def test_for_converter():
+    solution = Solution()
+    number = solution.romanToInt("XXIV")
+    assert number == 24
+
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        assert len(s) > 0
+
+        roman_settings = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+
+        integer_numbers_list = list(s)
+
+        for num, char in enumerate(integer_numbers_list):
+            integer_numbers_list[num] = roman_settings[char]
+
+        # [10, 10, 1, 5]
+        for i in range(len(integer_numbers_list) - 1):
+            if integer_numbers_list[i] < integer_numbers_list[i + 1]:
+                integer_numbers_list[i + 1] = integer_numbers_list[i +
+                                                                   1] - integer_numbers_list[i]
+                integer_numbers_list[i] = 0
+
+        return sum(integer_numbers_list)
+
+
+if __name__ == "__main__":
+    test_for_converter()
+    print("All tests passed")
