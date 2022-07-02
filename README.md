cff2535eded53893d0d22d8744dde2634b07af46
Ledaryy <andrew.ledary@gmail.com>
2022-07-02 11:05:44 +0100

    challenge 4 solved

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 1. [EASY]: "Roman to Integer", links: [folder](https://github.com/Ledaryy/challenges/tree/master/coding_challenges/1) || [leetcode](https://leetcode.com/problems/roman-to-integer/)
 2. [EASY]: "Merge Two Sorted Lists", links: [folder](https://github.com/Ledaryy/challenges/tree/master/coding_challenges/2) || [leetcode](https://leetcode.com/problems/merge-two-sorted-lists/)
 3. [EASY]: "Longest Common Prefix", links: [folder](https://github.com/Ledaryy/challenges/tree/master/coding_challenges/3) || [leetcode](https://leetcode.com/problems/longest-common-prefix/)
+4. [EASY]: "Valid Parentheses", links: [folder](https://github.com/Ledaryy/challenges/tree/master/coding_challenges/4) || [leetcode](https://leetcode.com/problems/valid-parentheses/)
---
+++ coding_challenges/4/main.py
@@ -0,0 +1,40 @@
+
+
+def test_for_solution():
+    solution = Solution()
+    answer = solution.isValid("()")
+    assert answer == True
+    answer = solution.isValid("()[]{}")
+    assert answer == True
+    answer = solution.isValid("(]")
+    assert answer == False
+    
+ 
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for char in s:
+            if char == "(" or char == "[" or char == "{":
+                stack.append(char)
+            elif char == ")" or char == "]" or char == "}":
+                if len(stack) == 0:
+                    return False
+                if char == ")" and stack[-1] == "(":
+                    stack.pop()
+                elif char == "]" and stack[-1] == "[":
+                    stack.pop()
+                elif char == "}" and stack[-1] == "{":
+                    stack.pop()
+                else:
+                    return False
+        return len(stack) == 0
+
+                
+                
+        
+if __name__ == "__main__":
+    try:
+        test_for_solution()
+        print("All tests passed")
+    except Exception as e:
+        print(e)
